dd4ebc8d42e667fb96bf115fed0e42e245d3d026
Justin Jongstra <00300061@rijnijssel.nl>
2024-01-09 08:52:53 +0100

    added cors_origin to env

--- middlewares/setupCors.js
+++ middlewares/setupCors.js
@@ -1,6 +1,6 @@
 function setupCors(req, res, next) {
-    res.header('Access-Control-Allow-Origin', 'http://localhost:5173');
-    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    res.header('Access-Control-Allow-Origin', process.env.CORS_ORIGIN);
+    res.header('Access-Control-Allow-Methods', 'GET');
     res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
     res.header('Access-Control-Allow-Credentials', 'true');
     next();
