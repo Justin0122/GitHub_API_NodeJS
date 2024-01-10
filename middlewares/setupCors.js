8d0cfc92eef8cf6cddec26c8a36fa3a11572800f
Justin Jongstra <00300061@rijnijssel.nl>
2024-01-10 20:25:50 +0100

    added origin check and return error if not allowed

--- middlewares/setupCors.js
+++ middlewares/setupCors.js
@@ -1,9 +1,14 @@
 function setupCors(req, res, next) {
-    res.header('Access-Control-Allow-Origin', process.env.CORS_ORIGIN);
-    res.header('Access-Control-Allow-Methods', 'GET');
-    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-    res.header('Access-Control-Allow-Credentials', 'true');
-    next();
+    let allowedOrigins = [process.env.CORS_ORIGIN];
+    if (allowedOrigins.includes(req.headers.origin)) {
+        res.header('Access-Control-Allow-Origin', process.env.CORS_ORIGIN);
+        res.header('Access-Control-Allow-Methods', 'GET');
+        res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+        res.header('Access-Control-Allow-Credentials', 'true');
+        next();
+    } else {
+        res.status(403).json({ message: 'Forbidden' });
+    }
 }
 
 module.exports = setupCors;
