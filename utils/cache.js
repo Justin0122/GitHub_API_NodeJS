9002c210924d5aeeae1c40eb12b284ee0fe6984f
Justin Jongstra <justinjongstra8@gmail.com>
2024-05-01 16:17:56 +0200

    changed cache from 24h to 2h

--- utils/cache.js
+++ utils/cache.js
@@ -1,7 +1,7 @@
 const NodeCache = require('node-cache');
 
 function createCache() {
-    return new NodeCache({ stdTTL: 86400 }); // Cache for 24 hours (in seconds)
+    return new NodeCache({ stdTTL: 7200 }); // Cache for 2 hours (in seconds)
 }
 
 module.exports = {
